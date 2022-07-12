6be8ed90b202fdef82ba2e061945c57cb67a69e2
Jarmo Hemminki <87070956+jarmo-lls@users.noreply.github.com>
2022-07-12 09:31:27 +0300

    Add basic integration tests

---
+++ tests/test_integration.py
@@ -0,0 +1,93 @@
+import unittest
+import os
+import requests
+import json
+
+
+API_URL = 'http://localhost:8000/process/'
+
+
+def call_api(endpoint, text):
+    url = API_URL + endpoint
+    headers = {'Content-Type': 'application/json'}
+    payload = json.dumps({"type": "text", "content": text})
+    return  requests.post(
+            url, headers=headers, data=payload).json()
+
+
+class TestTaggerIntegration(unittest.TestCase):
+
+    def setUp(self):
+        self.endpoint = "tagger"
+        self.text = "Filipas gyvena Vilniuje. "
+
+    def test_valid_response_type(self):
+        response = call_api(self.endpoint, self.text)
+        self.assertEqual(response["response"]["type"], 'annotations')
+
+    def test_api_response_content(self):
+        response = call_api(self.endpoint, self.text)
+        for entity in ("PROPN", "VERB", "PUNCT"):
+            self.assertIn(entity, response["response"]["annotations"])
+
+    def test_api_response_with_empty_request(self):
+        response = call_api(self.endpoint, "")
+        self.assertIn('annotations', response["response"])
+
+    def test_api_response_with_large_request(self):
+        large_text = self.text * 10000
+        response = call_api(self.endpoint, large_text)
+        self.assertEqual(response['failure']['errors'][0]['code'],
+                         'elg.request.too.large')
+
+    def test_api_response_with_long_token(self):
+        long_token = "A" * 10000
+        response = call_api(self.endpoint, long_token)
+        self.assertIn('annotations', response["response"])
+
+    def test_api_response_with_special_characters(self):
+        spec_text = "\N{grinning face}\u4e01\u0009" + self.text + "\u0008"
+        response = call_api(self.endpoint, spec_text)
+        gpe = response["response"]["annotations"]["PROPN"][1]
+        self.assertEqual(spec_text[gpe["start"]:gpe["end"]], "Vilniuje")
+
+
+class TestNERIntegration(unittest.TestCase):
+
+    def setUp(self):
+        self.endpoint = "ner"
+        self.text = "Filipas gyvena Vilniuje. "
+
+    def test_valid_response_type(self):
+        response = call_api(self.endpoint, self.text)
+        self.assertEqual(response["response"]["type"], 'annotations')
+
+    def test_api_response_content(self):
+        response = call_api(self.endpoint, self.text)
+        for entity in ("PERSON", "LOC"):
+            self.assertIn(entity, response["response"]["annotations"])
+
+    def test_api_response_with_empty_request(self):
+        response = call_api(self.endpoint, "")
+        self.assertIn('annotations', response["response"])
+
+    def test_api_response_with_large_request(self):
+        large_text = self.text * 1000
+        response = call_api(self.endpoint, large_text)
+        self.assertEqual(response['failure']['errors'][0]['code'],
+                         'elg.request.too.large')
+
+    def test_api_response_with_long_token(self):
+        long_token = "A" * 10000
+        response = call_api(self.endpoint, long_token)
+        self.assertIn('annotations', response["response"])
+
+    def test_api_response_with_special_characters(self):
+        spec_text = "\N{grinning face}\u4e01\u0009" + self.text + "\u0008"
+        response = call_api(self.endpoint, spec_text)
+        gpe = response["response"]["annotations"]["LOC"][0]
+        self.assertEqual(spec_text[gpe["start"]:gpe["end"]], "Vilniuje")
+
+
+if __name__ == '__main__':
+    unittest.main()
